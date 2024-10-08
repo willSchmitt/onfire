b7a93e861d05b8ee4c941a10b8ce8e7ecc2e0095
Willian Schmitt <w.schmitt2017@gmail.com>
2024-10-08 16:14:41 -0300

    resolved errors in git

--- src/app/components/Carousel/index.tsx
+++ src/app/components/Carousel/index.tsx
@@ -9,12 +9,10 @@ import image3 from "../../../../public/slide3.jpeg";
 import image4 from "../../../../public/slide4.jpeg";
 import image5 from "../../../../public/slide5.png";
 
-// Interface for image data
 interface ImageData {
   src: StaticImageData;
 }
 
-// Image data array
 const images: ImageData[] = [
   {
     src: image1,
@@ -34,45 +32,36 @@ const images: ImageData[] = [
 ];
 
 export default function ImageSlider(): JSX.Element {
-  // State to keep track of the current image index
   const [currentIndex, setCurrentIndex] = useState<number>(0);
 
-  // State to determine if the image is being hovered over
   const [isHovered, setIsHovered] = useState<boolean>(false);
 
-  // Function to show the previous slide
   const prevSlide = (): void => {
     setCurrentIndex(
       (prevIndex) => (prevIndex - 1 + images.length) % images.length
     );
   };
 
-  // Function to show the next slide
   const nextSlide = (): void => {
     setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
   };
 
-  // useEffect hook to handle automatic slide transition
   useEffect(() => {
-    // Start interval for automatic slide change if not hovered
     if (!isHovered) {
       const interval = setInterval(() => {
         nextSlide();
       }, 3000);
 
-      // Cleanup the interval on component unmount
       return () => {
         clearInterval(interval);
       };
     }
   }, [isHovered]);
 
-  // Handle mouse over event
   const handleMouseOver = (): void => {
     setIsHovered(true);
   };
 
-  // Handle mouse leave event
   const handleMouseLeave = (): void => {
     setIsHovered(false);
   };
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
-import Image from "next/image";
 
-import BgPage from "../public/bgPage.jpg";
 import "./globals.css";
 
 const geistSans = localFont({
