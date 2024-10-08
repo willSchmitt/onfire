b74c36fccd22d340f19a94787b153a01f86a73aa
Willian Schmitt <w.schmitt2017@gmail.com>
2024-10-08 17:10:35 -0300

    resolved errors

--- src/app/components/About/index.tsx
+++ src/app/components/About/index.tsx
@@ -4,8 +4,8 @@ export function AboutOnFire() {
       <div className="container mx-auto px-4 text-center">
         <h1 className="text-5xl font-bold mb-6">O QUE É O ONFIRE?</h1>
         <p className="text-xl text-gray-200 mb-10 max-w-2xl mx-auto">
-          A Conferência More Life OnFire 2024 está chegando com o tema "Chegará
-          o tempo em que não haverá mais tempo", e a vibe é trazer os movimentos
+          A Conferência More Life OnFire 2024 está chegando com o tema `Chegará
+          o tempo em que não haverá mais tempo`, e a vibe é trazer os movimentos
           do céu pra terra, agora! Esse é o momento de se conectar ao que Deus
           está fazendo e viver com intensidade total. Prepare-se para dias
           cheios de propósito, inspiração e muito fogo!
