c41af742deb49a4ebbf4f5bec3067712a4e7b94c
Willian Schmitt <w.schmitt2017@gmail.com>
2024-10-09 08:48:27 -0300

    resolved errors syntax

--- src/app/components/Programming/index.tsx
+++ src/app/components/Programming/index.tsx
@@ -1,7 +1,7 @@
 export function ProgrammingOnFire() {
   return (
     <div className="container mx-auto px-4 text-center flex flex-col gap-4 pt-28 pb-28 sm:flex-col max-md:pt-28">
-      <h1 className="text-4xl font-bold mb-6">PROGRAMAÇÃO </h1>
+      <h1 className="text-4xl font-bold mb-6">PROGRAMAÇÃO</h1>
       <div className="flex items-center justify-center gap-28 max-md:flex-col">
         <section>
           <h3 className="text-xl font-bold pb-8">SEXTA-FEIRA</h3>
@@ -48,16 +48,19 @@ export function ProgrammingOnFire() {
             19:00 | LOUVOR
           </p>
           <p className="text-xl text-gray-200 mb-10 max-w-2xl mx-auto">
-            19:10 PLENÁRIA 2
+            19:10 | PLENÁRIA 2
           </p>
           <p className="text-xl text-gray-200 mb-10 max-w-2xl mx-auto">
-            20:00 LOUVOR
+            20:00 | LOUVOR
           </p>
           <p className="text-xl text-gray-200 mb-10 max-w-2xl mx-auto">
-            20:30 PLENÁRIA 3
+            20:30 | PLENÁRIA 3
           </p>
           <p className="text-xl text-gray-200 mb-10 max-w-2xl mx-auto">
-            21:30 LOUVOR | ENCERRAMENTO ás 22:00
+            21:30 | LOUVOR
+          </p>
+          <p className="text-xl text-gray-200 mb-10 max-w-2xl mx-auto">
+            22:00 | ENCERRAMENTO
           </p>
         </section>
       </div>
